d659153c71199714c3f3bdc12260022058c98f57
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-07-23 14:46:51 +0530

    Create loops.dart

---
+++ loops.dart
@@ -0,0 +1,40 @@
+void main() {
+  
+    // for loop
+//     for(var i=1;i<=20;i++){
+//       if(i % 2 == 0){
+//         print(i);
+//       }
+      
+//     }
+  
+  print("********************");
+  
+  // for .. in loop
+//   List planetList = ["Mercury","Venus","Earth","Mars"];
+  
+//   for (String planet in planetList) {
+//     print(planet);
+//   }
+  
+  // while loop
+//   var i = 1;
+//   while (i <= 10) {
+        
+//       if(i%2==0){
+//       print(i);
+//       }
+//       i++;
+//   }
+  
+  
+  // do while loop
+  
+  int i = 1;
+  do {
+    if(i%2==0){
+        print(i);
+  }
+  i++;
+  } while(i <= 10);
+}
