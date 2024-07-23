ca163151b8e5089b8eebb4704cbba0c0bc324a25
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-07-23 11:18:16 +0530

    Create var.dart

---
+++ var.dart
@@ -0,0 +1,57 @@
+/* Data types
+
+  Number
+    int, double
+    
+    int age=10;
+    var age=12; it will consider as integer automatically
+    
+  Strings
+  
+    String name = "Henry";
+    var name = "Henry";
+    
+  Booleans
+  
+    bool isAlive = true;
+    var isAlive = false;
+    
+  Lists(aka Arrays)
+  Maps
+  Runes(for Unicode characters in a String)
+  Symbols
+  
+  All data types in Dart are objects
+  Default value of all are null
+  
+*/ 
+
+void main() {
+  
+  int score = 24;
+  var count = 24;   // it will consider it as integer
+  
+  int hexValue = 0xEADABAEE;
+  
+  double percent = 98.23;
+  
+  double expo = 1.42e5;
+  
+  
+  String name = "Henry";
+  var nameOfPerson = "Henry";
+  
+  bool isValid = true;
+  
+  print(score);
+  print(count);
+  print(nameOfPerson);
+  print(percent);
+  
+  print(expo);
+  print(hexValue);
+  
+  print(name);
+  
+  
+}
