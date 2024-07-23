46615035183e8fd124633812bb15512d14198db6
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-07-23 10:58:19 +0530

    Create basic.dart

---
+++ basic.dart
@@ -0,0 +1,22 @@
+/*
+ * void main() {
+  for (int i = 0; i < 10; i++) {
+    print('hello ${i + 1}');
+  }
+}
+*/
+
+// Entry point is main in dart
+
+void main() {
+  
+  print("Hey there!");
+  
+  print("Hi I am a bot!");
+  
+  print(1 - 1);
+  
+  print(true);
+    
+}
+
