ff90b39473edd1e1f08a43aaa4b1f2f182503742
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-07-24 12:02:53 +0530

    Create exceptions.dart

---
+++ exceptions.dart
@@ -0,0 +1,40 @@
+// User defined exception
+
+ void main(){
+   
+   // Case -5
+   // Custom Exception
+   print("*********");
+   try{
+   depositMoney(-200);
+   } catch(e){
+     if(e is DepositException){
+     print(e.errorMessage());
+     }
+     else{
+       print("Unknown error: $e");
+     }
+   } finally{
+     print("This piece of code is always executed.");
+   }
+   //on DepositException {
+    // print("The error message is :");
+    // print(e.errorMessage());
+  // }
+   
+ }
+
+
+class DepositException implements Exception {
+  
+  String errorMessage() {
+//     print("You cant enter an amount less than 0");
+    return "You cant enter an amount less than 0";
+  }
+}
+
+void depositMoney(int amt){
+  if(amt < 0) {
+    throw new DepositException();
+  }
+}
