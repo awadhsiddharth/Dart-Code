209648a42a99227d9835fc6fcc4e89998acf45c3
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-07-24 12:34:14 +0530

    Create class1.dart

---
+++ class1.dart
@@ -0,0 +1,42 @@
+// define states and behavior of a student
+
+class Student{
+
+  // instance or field value
+  // it has default value of null
+int id= -1;
+String? name;
+
+void study() {
+  print("${this.name} is studying...");
+}
+
+void sleep() {
+  print("${this.name} is sleeping  now....");
+}
+}
+
+void main() {
+  
+  // while creating an obj new keyword is optional
+  var student1 = Student();
+  // Student student1 = new Student();
+  student1.id = 23;
+  student1.name = "Harry";
+  
+  print("${student1.id} and ${student1.name}");
+  student1.study();
+ 
+  print("");
+  print("****************");
+  print("");
+  
+  var stud2 = Student();
+  stud2.id = 24;
+  stud2.name = "Samuel";
+ 
+  print("${stud2.id} and ${stud2.name}");
+  stud2.study();
+ 
+  
+}
